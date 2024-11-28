941580e21ba1e8049aca2243afcc399d367966f1
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2024-11-28 17:23:44 +0200

    Test: Add fixtures

--- tests/tests/test_always_true.py
+++ tests/tests/test_always_true.py
@@ -2,5 +2,7 @@ from django.test import TestCase
 
 
 class AlwaysTrueTestCase(TestCase):
+    fixtures = ["users", "profiles", "posts"]
+
     def test_always_true(self):
         return self.assertTrue(True)
